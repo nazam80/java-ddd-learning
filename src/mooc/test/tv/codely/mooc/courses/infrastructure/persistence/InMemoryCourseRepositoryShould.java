81ea87eb9c8b043bc269679235e826808a7b3590
Antonio Martínez López <nazam80@gmail.com>
2020-02-12 12:47:26 +0100

    Added semantics to infrasctructure tests

--- src/mooc/test/tv/codely/mooc/courses/infrastructure/persistence/InMemoryCourseRepositoryShould.java
+++ src/mooc/test/tv/codely/mooc/courses/infrastructure/persistence/InMemoryCourseRepositoryShould.java
@@ -2,6 +2,7 @@ package tv.codely.mooc.courses.infrastructure.persistence;
 
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import tv.codely.mooc.courses.CourseModuleInfrastructureTestCase;
 import tv.codely.mooc.courses.domain.Course;
 import tv.codely.mooc.courses.domain.CourseId;
 import tv.codely.mooc.courses.domain.CourseIdMother;
@@ -9,11 +10,10 @@ import tv.codely.mooc.courses.domain.CourseMother;
 
 import java.util.Optional;
 
-class InMemoryCourseRepositoryShould {
+public class InMemoryCourseRepositoryShould extends CourseModuleInfrastructureTestCase {
 
     @Test
     void save_a_valid_course() {
-        InMemoryCourseRepository repository = new InMemoryCourseRepository();
         Course course = CourseMother.random();
         repository.save(course);
     }
@@ -21,7 +21,6 @@ class InMemoryCourseRepositoryShould {
 
     @Test
     void search_an_existing_course() {
-        InMemoryCourseRepository repository = new InMemoryCourseRepository();
         Course course = CourseMother.random();
         repository.save(course);
 
@@ -30,7 +29,6 @@ class InMemoryCourseRepositoryShould {
 
     @Test
     void search_not_existing_course() {
-        InMemoryCourseRepository repository = new InMemoryCourseRepository();
         CourseId id = CourseIdMother.random();
         Assert.assertFalse(repository.search(id).isPresent());
     }
